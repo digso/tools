0292ee7efb0256e79307a18c72a630ebd61f68e8
James Aprosail <aprosail@outlook.com>
2024-09-15 22:51:52 +0800

    repo apply lints from relative path

--- monorepo/bin/monorepo.dart
+++ monorepo/bin/monorepo.dart
@@ -1,5 +1,1 @@
-import 'package:monorepo/monorepo.dart' as monorepo;
-
-void main(List<String> arguments) {
-  print('Hello world: ${monorepo.calculate()}!');
-}
+void main(List<String> arguments) {}
