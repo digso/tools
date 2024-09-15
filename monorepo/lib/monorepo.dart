b7f3e0915f14cf23bba229b26be6986997a1dcbb
James Aprosail <aprosail@outlook.com>
2024-09-15 23:37:23 +0800

    chore record define data structure

--- monorepo/lib/monorepo.dart
+++ monorepo/lib/monorepo.dart
@@ -1,1 +1,17 @@
+import 'dart:io';
 
+class Monorepo {
+  const Monorepo(this.root);
+
+  final Directory root;
+
+  Future<void> test() async {}
+
+  Future<void> review() async {}
+
+  Future<void> publish() async {}
+
+  Future<void> pubIgnore() async {}
+
+  Future<void> resolveVersions() async {}
+}
