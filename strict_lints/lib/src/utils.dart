0d756fbe0805ceeb053bc117074cf590f3360459
James Aprosail <aprosail@outlook.com>
2024-09-15 14:33:07 +0800

    feat compute all combinations of a set

--- strict_lints/lib/src/utils.dart
+++ strict_lints/lib/src/utils.dart
@@ -15,3 +15,36 @@ extension StringUtils on String {
     return length == 1 ? first : first + substring(1).toLowerCase();
   }
 }
+
+extension AllCombinations<T> on Set<T> {
+  Set<Set<T>> get allCombinations => {{}, ...allCombinationsWithoutEmpty};
+
+  Set<Set<T>> get allCombinationsWithoutEmpty {
+    final initValue = {
+      for (final item in this) {item},
+    };
+    final handler = [initValue];
+    var currentLayer = initValue;
+    for (int i = 1; i < length; i++) {
+      final nextLayer = <Set<T>>{};
+      for (final items in currentLayer) {
+        for (final item in this) {
+          if (items.contains(item)) continue;
+          final current = {...items, item};
+
+          var repeat = false;
+          for (final already in nextLayer) {
+            if (already.containsAll(current)) {
+              repeat = true;
+              continue;
+            }
+          }
+          if (!repeat) nextLayer.add(current);
+        }
+      }
+      handler.add(nextLayer);
+      currentLayer = nextLayer;
+    }
+    return {for (final layer in handler) ...layer};
+  }
+}
---
+++ strict_lints/test/utils_test.dart
@@ -0,0 +1,27 @@
+import 'package:strict_lints/spider.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('all combinations', () {
+    final values = {1, 2, 3, 4};
+    final allCombinations = <Set<int>>{
+      {},
+      {1},
+      {2},
+      {3},
+      {4},
+      {1, 2},
+      {1, 3},
+      {1, 4},
+      {2, 3},
+      {2, 4},
+      {3, 4},
+      {1, 2, 3},
+      {1, 2, 4},
+      {1, 3, 4},
+      {2, 3, 4},
+      {1, 2, 3, 4},
+    };
+    expect(values.allCombinations, allCombinations);
+  });
+}
